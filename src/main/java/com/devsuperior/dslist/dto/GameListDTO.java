237b694169c061042d3b9804e0339661a344a40a
caioo <matossousacaio.cms@gmail.com>
2024-11-12 19:35:49 -0300

    get lists

---
+++ src/main/java/com/devsuperior/dslist/dto/GameListDTO.java
@@ -0,0 +1,22 @@
+package com.devsuperior.dslist.dto;
+
+import org.springframework.beans.BeanUtils;
+
+import com.devsuperior.dslist.entities.GameList;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GameListDTO {
+    private Long id;
+    private String name;
+
+    public GameListDTO(){
+    }
+
+    public GameListDTO(GameList list){
+        BeanUtils.copyProperties(list, this);
+    }
+}
